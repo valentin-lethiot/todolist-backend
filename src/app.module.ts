e4216799e4e2c27c434d0f810f8b7e2d7fe2e84a
valentinlth <valentin.lethiot@gmail.com>
2025-02-25 17:23:06 +0100

    :sparkles: end of features

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './app/tasks/tasks.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
     imports: [
+        ConfigModule.forRoot(),
         TasksModule,
-        MongooseModule.forRoot('mongodb://admin:secret@localhost:27017/nestdb?authSource=admin'),
+        MongooseModule.forRoot(process.env.MONGO_URI || 'default_mongo_uri'),
     ],
 })
 export class AppModule {}
